987308c23d4c9061c0ad42da5c7c664cd1ffa44f
dharnishp <128043459+dharnishp@users.noreply.github.com>
2025-02-08 20:15:20 +0530

    Add files via upload

---
+++ Program7.java
@@ -0,0 +1,30 @@
+package arrayInterviewQuestions;
+
+import java.util.Scanner;
+
+public class Program7 
+{
+	public static void main(String[] args) 
+	{
+		Scanner scn=new Scanner(System.in);
+		int n=scn.nextInt();
+		int[] arr=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=scn.nextInt();
+		}
+		int large=arr[0],small=arr[0];
+		for(int i=0;i<n;i++)
+		{
+			if(arr[i]>large)
+			{
+				large=arr[i];
+			}
+			if(arr[i]<small)
+			{
+				small=arr[i];
+			}
+		}
+		System.out.println("Largest number in array : "+large+"\nSmallest number in array : "+small);
+	}
+}
---
+++ Program8.java
@@ -0,0 +1,40 @@
+package arrayInterviewQuestions;
+
+import java.util.LinkedHashMap;
+import java.util.Scanner;
+import java.util.Set;
+public class Program8 
+{
+	public static void main(String[] args) 
+	{
+		Scanner scn=new Scanner(System.in);
+		int n=scn.nextInt();
+		int[] arr =new int[n];
+		LinkedHashMap<Integer,Integer> lhm=new LinkedHashMap();
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=scn.nextInt();
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(lhm.containsKey(arr[i]))
+			{
+				lhm.put(arr[i], lhm.get(arr[i])+1);
+			}
+			else
+			{
+				lhm.put(arr[i], 1);
+			}
+		}
+		int multiple=1;
+		Set<Integer> s=lhm.keySet();
+		for(int i:s)
+		{
+			if(lhm.get(i)>1)
+			{
+				multiple*=i;
+			}
+		}
+		System.out.println(multiple);
+	}
+}
