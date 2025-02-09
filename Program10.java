d4c460524144f4db3c9cbdf4765e58f708ac3f0a
dharnishp <128043459+dharnishp@users.noreply.github.com>
2025-02-09 17:54:37 +0530

    Add files via upload

---
+++ Program10.java
@@ -0,0 +1,52 @@
+package arrayInterviewQuestions;
+
+import java.util.Scanner;
+
+public class Program10 
+{
+	public static void main(String[] args) 
+	{
+		Scanner scn=new Scanner(System.in);
+		int n=scn.nextInt();
+		int[] arr=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=scn.nextInt();
+		}
+		int temp=0;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=i+1;j<n;j++)
+			{
+				if(arr[i]>arr[j])
+				{
+					temp=arr[i];
+					arr[i]=arr[j];
+					arr[j]=temp;
+				}
+			}
+		}
+		System.out.println("Ascending order");
+		for(int i:arr)
+		{
+			System.out.print(i+" ");
+		}
+		for(int i=0;i<n;i++)
+		{
+			for(int j=i+1;j<n;j++)
+			{
+				if(arr[i]<arr[j])
+				{
+					temp=arr[i];
+					arr[i]=arr[j];
+					arr[j]=temp;
+				}
+			}
+		}
+		System.out.println("\nDescending");
+		for(int i:arr)
+		{
+			System.out.print(i+" ");
+		}
+	}
+}
