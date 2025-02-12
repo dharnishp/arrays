3cfe8756f24b65a914102640fd071cf804864b3f
dharnishp <128043459+dharnishp@users.noreply.github.com>
2025-02-12 21:39:25 +0530

    Add files via upload

---
+++ Program16.java
@@ -0,0 +1,24 @@
+package arrayInterviewQuestions;
+
+public class Program16 {
+	public static void main(String[] args) {
+		int[] arr1={10,20,30,40,50};
+		int[] arr2={1000,500,100,200,300};
+		int[] mergeArr=new int[arr1.length+arr2.length];
+		for(int i=0;i<arr1.length+arr2.length;i++)
+		{
+			if(i<arr1.length)
+			{
+				mergeArr[i]=arr1[i];
+			}
+			else
+			{
+				mergeArr[i]=arr2[i-arr1.length];
+			}
+		}
+		for(int i:mergeArr)
+		{
+			System.out.print(i+" ");
+		}
+	}
+}
