a1932f63dec4afb4779b32de1d2041e03aec48a2
dharnishp <128043459+dharnishp@users.noreply.github.com>
2025-02-13 21:40:09 +0530

    Add files via upload

---
+++ Program18.java
@@ -0,0 +1,32 @@
+package arrayInterviewQuestions;
+
+import java.util.Scanner;
+
+public class Program18
+{
+	public static void main(String[] args) 
+	{
+		Scanner scn=new Scanner(System.in);
+		int n=scn.nextInt();
+		int[] arr=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=scn.nextInt();
+		}
+		int evenCount=0,oddCount=0;
+		for(int i=0;i<n;i++)
+		{
+			if(i%2==0)
+			{
+				evenCount++;
+			}
+			else
+			{
+				oddCount++;
+			}
+		}
+		System.out.println("Count of Even Elements in array : "+evenCount);
+		System.out.println("Count of Odd Elements in array : "+oddCount);
+	}
+}
+
